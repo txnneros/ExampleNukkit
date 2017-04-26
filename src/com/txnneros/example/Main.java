63a47b66ba16370d3682b3727cec8a935ff39361
TxnnerOS <redrealmmc@gmail.com>
2017-04-26 12:15:39 -0400

    Update Main.java

--- src/com/txnneros/example/Main.java
+++ src/com/txnneros/example/Main.java
@@ -1,1 +1,11 @@
 
+package com.txnneros.example;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+    public void onEnable() {
+        getLogger().info("ExampleNukkit plugin enabled!!");
+    }
+}
