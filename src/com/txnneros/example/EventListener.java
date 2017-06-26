11421483c4b406ece6e4c990dd662108668ea128
TxnnerOS <redrealmmc@gmail.com>
2017-06-26 00:00:13 -0400

    Update EventListener.java

--- src/com/txnneros/example/EventListener.java
+++ src/com/txnneros/example/EventListener.java
@@ -1,1 +1,26 @@
+package com.txnneros.example;
 
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
+
+public class EventListener implements Listener {
+  
+    Main plugin;
+
+    public EventListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+    public void onJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+    }
+    
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+    public void onQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(null);
+    }
+}
